e408836392cfb969775bf20cc9536c5eaa7c6d14
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-06-13 07:28:18 +0300

    Assigning id for each category image + Saving its category upon clicking

--- frontend-user/scripts/category.js
+++ frontend-user/scripts/category.js
@@ -11,6 +11,14 @@ if(name_from_storage != null){
     }
 }
 
+// Variables to get the category clicked
+let required_category = ""
+let listing_category_names = [];
+let listing_category_ids = [];
+let img = "img"
+let counter = 1;
+
+// Sending get request to get all categories in db
 axios.get("http://127.0.0.1:8000/api/v1/user/category_search")
 .then(res => display(res))
 .catch(e=> console.log(e));
@@ -25,10 +33,27 @@ function display(res){
         let category_name = document.createElement("p");
         category_name.innerHTML = result[i].category_name;
         categories.appendChild(category_name);
+        listing_category_names.push(result[i].category_name);
         // Inserting the image
         let image = document.createElement("img");
+        image.id = img +counter;
+        listing_category_ids.push(image.id);
         image.classList.add('item');
         image.src = result[i].images;
         categories.appendChild(image);
+        counter++;
+    }
+
+    // Saving the clicked category to get its items
+    for (let i=0; i < result.length; i++){
+        for (let i=0; i < result.length; i++){
+            document.getElementById(listing_category_ids[i]).onclick=showItemByCategory;
+            function showItemByCategory(){
+                required_category = listing_category_names[i]
+                console.log(required_category)
+                let url1 = './items.html?category_name=' + required_category;
+                window.location.href = url1;
+            }
+        }
     }
 }
