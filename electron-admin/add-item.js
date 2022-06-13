c6be9d9da0dbc11013a0f9cb8c074be95fafa1de
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-06-13 03:46:20 +0300

    Letting the user to choose pre-defined category upon adding new item

--- electron-admin/add-item.js
+++ electron-admin/add-item.js
@@ -2,12 +2,33 @@
 let item_name = document.getElementById("item-name");
 let item_description = document.getElementById("item-description")
 let item_category = document.getElementById("item_category")
+let categories = []
 
 let add_Item = document.getElementById("add-item-btn")
 
 // Getting the token from the localstorage
 let admin_token = localStorage.getItem('access_token');
 
+// Getting the pre-added categegories
+axios.get("http://127.0.0.1:8000/api/v1/user/category_search")
+.then(res => {showCategories(res)})
+.catch(e=> console.log(e));
+
+function showCategories(res){
+    let response = res.data.results;
+    // Storing the categories in an array called categories
+    for(let i=0; i< response.length; i++){
+        categories.push(response[i].category_name)
+    }
+    // Adding the categories found in db to the select field
+    for (let i = 0; i < response.length; i++){
+        let option = document.createElement("option");
+        option.innerHTML = response[i].category_name;
+        item_category.appendChild(option);
+    }
+}
+
+// Adding the new item to the db
 add_Item.onclick = addItem
 
 function addItem(){
