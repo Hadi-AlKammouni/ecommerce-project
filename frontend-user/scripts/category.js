a117fa3de93f14a695c1a3965f89dd8509f62b39
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-06-13 06:09:58 +0300

    Inserting category details from db

--- frontend-user/scripts/category.js
+++ frontend-user/scripts/category.js
@@ -1,5 +1,6 @@
 // Declaring required variables
 let name_from_storage = localStorage.getItem("name");
+let categories = document.getElementById("items-display");
 
 // Check if the user is logged in or no/ if yes, add greeting
 if(name_from_storage != null){
@@ -11,5 +12,23 @@ if(name_from_storage != null){
 }
 
 axios.get("http://127.0.0.1:8000/api/v1/user/category_search")
-.then(res => {console.log(res.data)})
+.then(res => display(res))
 .catch(e=> console.log(e));
+
+function display(res){
+    let result = res.data.results;
+    console.log(result);
+
+    // Displaying the categories
+    for (let i=0; i < result.length; i++){
+        // Inserting the category name
+        let category_name = document.createElement("p");
+        category_name.innerHTML = result[i].category_name;
+        categories.appendChild(category_name);
+        // Inserting the image
+        let image = document.createElement("img");
+        image.classList.add('item');
+        image.src = result[i].images;
+        categories.appendChild(image);
+    }
+}
