661cbcbf6f5c688b19fba034d6f9f231bd7c37f1
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-06-13 09:20:17 +0300

    Implementing add favourite functionality which is restricted for logged in users

--- frontend-user/scripts/items.js
+++ frontend-user/scripts/items.js
@@ -3,6 +3,10 @@ let name_from_storage = localStorage.getItem("name");
 let items_title = document.getElementById("items-title");
 let items = document.getElementById("items-display");
 
+// Getting the token from the localstorage
+let user_token = localStorage.getItem('access_token');
+let id = localStorage.getItem('user_id');
+
 // Getting the clicked category
 const urlSearchParams = new URLSearchParams(window.location.search);
 var category_name = Object.fromEntries(urlSearchParams.entries());
@@ -20,6 +24,8 @@ if(name_from_storage != null){
 // To add id for each item image
 let item = "item";
 let counter = 1;
+let item_db_ids = []
+let item_html_id = []
 
 // Changing the subtitle into required category
 items_title.innerText = category_name  
@@ -45,9 +51,38 @@ function display(res){
         image.classList.add('item');
         image.src = result[i].images;
         items.appendChild(image);
+        item_html_id.push(image.id)
         counter++;
+        
+        // Storing ids of items in item_db_ids list
+        item_db_ids.push(result[i].item_id);
+    }
+
+    for (let i=0; i < item_db_ids.length; i++){
+        console.log(item_db_ids)
+        console.log(item_html_id)
+        console.log(user_token)
+        document.getElementById(item_html_id[i]).onclick = addToFavourites;
+        function addToFavourites(){
+            // Sending post request
+            let data = new FormData();
+            data.append('id', id);
+            data.append('item_id', item_db_ids[i]);
+
+            // Send request POST
+            axios.post("http://127.0.0.1:8000/api/v1/user/add_favourite",data, {
+                headers:{
+                    Authorization: "Bearer " + user_token,
+                    Accept: "application/json"}})
+            .then((response) => {
+                alert("Item Added To Favourites Successfully"),
+                console.log(response.data)
+            })
+            .catch(e=>{
+                if(e.response.status != 200){
+                    alert(e.response.data.message);
+                }
+            })
+        }
     }
 }
-
-// Getting the token from the localstorage
-let admin_token = localStorage.getItem('access_token');
